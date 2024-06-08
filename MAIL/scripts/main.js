350df3e7145c86f89560f02fe6fa4f440e8b09a0
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-08 04:54:33 +0200

    feat(MAIL); project completed

--- MAIL/scripts/main.js
+++ MAIL/scripts/main.js
@@ -1,6 +1,5 @@
 // Allowed users mail addresses' list
 const allowedMails = ['josephineberg@capscreen.com' , 'sheltonweber@capscreen.com' , 'battlemiles@capscreen.com' , 'adelesheppard@capscreen.com' , 'knightcarey@capscreen.com'];
-
 const logForm = document.getElementById('log-form');
 
 // On form submition start the function to find if the access is granted
@@ -14,6 +13,7 @@ logForm.addEventListener('submit' ,
             allowedMails[i] === userEmail ? isInTheList = true : null;
         }
         
+        // Print different messages if access in confirmed or denied
         isInTheList === false ? document.getElementById('output').innerHTML = `<strong class="denied">ACCESS DENIED</strong><p>Sorry, <span>${userEmail}</span> does not correspond to any of the emails authorized to access this document!</p>` : document.getElementById('output').innerHTML = `<strong class="granted">ACCESS GRANTED</strong><p>Hello, <span>${userEmail}</span> is authorized to access this document!</p>`;
     }
 );
